7418eef9a7a1a7c5d0be3927dab2808308426a6e
saglamorhan61 <saglamorhan.61@gmail.com>
2020-12-25 18:36:47 +0300

    Initial commit

---
+++ app/src/main/java/com/saglamorhan/retrofitprojekotlin/retrofitkotlin/view/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.saglamorhan.retrofitprojekotlin.retrofitkotlin.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.saglamorhan.retrofitprojekotlin.retrofitkotlin.R
+
+class MainActivity : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+}
